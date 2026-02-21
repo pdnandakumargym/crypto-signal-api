f377484cb91a22f5bc102c1e7211de840939e00c
NandaKumar <pdnandakumargym@gmail.com>
2026-02-21 20:03:01 +0530

    binance live fix

--- src/services/binance.service.ts
+++ src/services/binance.service.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const getCandles = async (symbol: string) => {
-  const res = await axios.get("https://api.binance.com/api/v3/klines", {
+  const res = await axios.get(process.env.BINANCE_API_KEY || "https://api.binance.com/api/v3/klines", {
     params: {
       symbol,
       interval: "15m",
